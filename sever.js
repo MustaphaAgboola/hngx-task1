fdaaaf5338e027d4cc25fcc4c4cc90bc17c4447b
Mustapha Agboola <mustaphaagboola1@yahoo.com>
2023-09-08 16:41:56 +0100

    Fixing other utc date

--- sever.js
+++ sever.js
@@ -13,12 +13,10 @@ const days = [
 
 const currentDay = days[new Date().getDay()];
 
-const now = new Date();
-
 const utc = new Date(
   new Date().getTime() + new Date().getTimezoneOffset() * 60000
 );
-const utcTime = utc.toISOString().split(".")[0] + "Z";
+const utcTime = utc.toISOString().split('.')[0] + 'Z';
 
 app.get("/api", (request, response) => {
   response.status(200).json({
